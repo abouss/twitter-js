cc4d5d0a0e5d337d1d3d74e76325eeeee0e90126
Anfal Boussayoud <anfal.boussayoud@gmail.com>
2015-02-12 15:30:53 -0500

    updated app.js

--- app.js
+++ app.js
@@ -1,3 +1,37 @@
 var express = require('express');
+var swig = require('swig');
+var morgan = require('morgan');
+
 var app = express();
+
+
+app.engine('html', require('swig').renderFile);
+
+app.set('view engine', 'html');
+
+app.set('views', __dirname + './views');
+
+
+swig.setDefaults({cache: false});
+
+
+
+app.use(morgan('dev'));
+
+app.get('/', function(req, res) {
+	res.send('gosh damned!');
+});
+
+
+var server = app.listen(3000, function() {
+
+	var host = server.address().address;
+	var port = server.address().port;
+
+	console.log('Example app listening at http://%s%s', host ,port);
+});
+
+
+
+
 
