29fa76ac3673469ccfe034c57705a6a9d7767cdb
Anfal Boussayoud <anfal.boussayoud@gmail.com>
2015-02-14 17:48:44 -0500

    updated style sheets

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var swig = require('swig');
 var morgan = require('morgan');
+var routes = require('./routes/');
+
 
 var app = express();
 
@@ -9,7 +11,7 @@ app.engine('html', require('swig').renderFile);
 
 app.set('view engine', 'html');
 
-app.set('views', __dirname + './views');
+app.set('views', __dirname + '/views');
 
 
 swig.setDefaults({cache: false});
@@ -18,9 +20,17 @@ swig.setDefaults({cache: false});
 
 app.use(morgan('dev'));
 
-app.get('/', function(req, res) {
-	res.send('gosh damned!');
-});
+app.use('/', routes);
+
+app.use(express.static(__dirname + '/public'));
+
+
+// app.get('/', function(req, res) {
+// 	var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+// 	res.render( 'index', {title: 'Hall of ME', people: people} );
+// });
+
+
 
 
 var server = app.listen(3000, function() {
